3ce68d8252c41fffae3113ea51b967866f3df206
Daniyalatjit <Daniyalatjit@users.noreply.github.com>
2020-11-03 23:38:34 +0530

    Social Network Analysis

--- Network Analysis/network-analysis.py
+++ Network Analysis/network-analysis.py
@@ -10,6 +10,7 @@
 # the creation, manipulation and study of the structure, dynamics and functiosn
 # of complex networks.
 import networkx as nx
+import matplotlib.pyplot as plt
 
 # Basic use of NetwrokX
 # I'm creating a graph as follows:
@@ -27,3 +28,21 @@ G.add_edge('D', 'E')
 
 # Here the graph is printed
 nx.draw_networkx(G)
+
+# Note: for creating directed graph nx.DiGraph is used
+
+# For this project I've cited dataset from e Stanford Large Network Dataset (SNAP)
+# This dataset consists of a network representating friendship between Facebook users.
+# The Facebook data has been anonymized by replacing the internal Facebook identifiers
+# for each user with a new value
+
+# let us load and extract some basic information from the graph./
+fb = nx.read_adjlist("facebook_combined.txt")
+fb_n, fb_k = fb.order(), fb.size()
+fb_avg_deg = fb_k / fb_n
+print('Nodes: ', fb_n)
+print('Edges: ', fb_k)
+print('Average Degree: ', int(fb_avg_deg))
+degrees = [v for k, v in fb.degree() ]
+degree_hist = plt.hist(degrees, 100)
+plt.plot(degree_hist[0], degree_hist[1])
