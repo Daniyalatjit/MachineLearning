5421e0d9a3c369d2319a2e47fa66a9f0ee5f5f41
Daniyalatjit <Daniyalatjit@users.noreply.github.com>
2020-11-05 00:52:13 +0530

    Social Network Analysis

--- Network Analysis/network-analysis.py
+++ Network Analysis/network-analysis.py
@@ -12,6 +12,10 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 from networkx.algorithms import centrality
+from networkx.algorithms.centrality import betweenness
+from networkx.algorithms.centrality import closeness
+from networkx.algorithms.centrality import eigenvector
+from networkx.generators.geometric import thresholded_random_geometric_graph
 from networkx.generators.small import make_small_undirected_graph
 
 # Basic use of NetwrokX
@@ -75,14 +79,49 @@ print('Size of the connected components: ', [len(c) for c in fb_components])
 # most informed and most communicative.
 # There are for best-known measures of centrality:
 # 1-    Degree Centrality
-# 2-    Betweeness Centrality
+# 2-    Betweenness Centrality
 # 3-    Closness Centrality
 # 4-    Eigenvector Centrality
 # read more in the document....
 
+# Degree Centrality
 degree_cnet_fb = nx.degree_centrality(fb)
 print('Facebook degree centrality: ', sorted( degree_cnet_fb.items(), 
                                     key= lambda x: x[1],
                                     reverse= True)[:10])
 degree_hist = plt.hist(list(degree_cnet_fb.values()), 100)
 plt.loglog(degree_hist[1][1:], degree_hist[0], 'b', marker= 'o')
+
+# Betweenness, Closeness and Eigenvector Centrality
+# Note this part may take a while to be executed depending on your systems speed.
+# betweenness_fb = nx.betweenness_centrality(fb)
+# closeness_fb = nx.closeness_centrality(fb)
+# eigenvector_fb = nx.eigenvector_centrality(fb)
+# print("Betweenness Centrality: ", sorted(betweenness_fb.items(),
+#                                     key= lambda x:x[1],
+#                                     reverse=True)[:10])
+# print("Closeness Centrality: ", sorted(closeness_fb.items(),
+#                                     key= lambda x:x[1],
+#                                     reverse=True)[:10])
+# print("Eigenvector Centrality: ", sorted(eigenvector_fb.items(),
+#                                     key= lambda x:x[1],
+#                                     reverse=True)[:10])
+
+# If I only consider the graph nodes with more than the average degree of
+# the network, I will do it by trimming the graph using degree centrality 
+# value.
+# I will define a function to trim the graph based of degree centrality value
+# I will set the threshold to 21 connections.
+
+def trim_by_degree_centrality(graph, degree = 0.01):
+    gr = graph.copy()
+    d = nx.degree_centrality(gr)
+    for n in list(gr.nodes()):
+        if d[n] <= degree:
+            gr.remove_node(n)
+    return gr
+
+degree_centrality_threshold = 21.0/(fb.order()-1.0)
+print('Degree Centrality Threshold: ', degree_centrality_threshold)
+fb_trimmed = trim_by_degree_centrality(fb, degree=degree_centrality_threshold)
+print('Remaing Number of Nodes: ', len(fb_trimmed))
